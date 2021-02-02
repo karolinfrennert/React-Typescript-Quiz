a05f96e87f902847e8f0009c1e3e1b2fce437af5
karolinfrennert <karolinfrennert@gmail.com>
2021-02-02 21:22:50 +0100

    API connection working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {fetchQuizQuestions, Difficulty} from './Api'
 
-function App() {
+ 
+
+//components
+import QuestionCard from './components/QuestionCard'
+const TOTAL_QUESTIONS = 10;
+
+
+
+
+const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <h1>Rect Quiz</h1>  
+    <button className="start" onClick={startTrivia}>Start quiz</button>
+    <p className="score"> Score:</p>
+    <p>Loading Questions...</p>
+    {/*<QuestionCard
+    questionNumber={number + 1}
+    totalQuestions={TOTAL_QUESTIONS}
+    question={questions[number].questions}
+    answers={questions[number].answers}
+    userAnswer={userAnswers ? userAnswers[number] : undefined}
+    callback={checkAnswer}
+    
+    
+    
+    />*/}
+    <button className="next" onClick={nextQuestion}>Next question</button>
     </div>
   );
 }
